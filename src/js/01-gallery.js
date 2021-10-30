ae24643da8da75e92de44411558590516e95cf03
Alina <alinasuv@gmail.com>
2021-10-30 22:27:54 +0300

    Добавляет 1 задание

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+const imgContainer = document.querySelector('.gallery');
+const imgMarkup = createImgCardsMarkUp(galleryItems);
+
+imgContainer.insertAdjacentHTML('beforeend', imgMarkup);
+
+imgContainer.addEventListener('click', onImgContainerClick)
+
+function createImgCardsMarkUp (images){
+return images.map ( ({preview, original, description}) => {
+return `
+<a class="gallery__item" href="${original}">
+<img class="gallery__image" data-source = "${original}" src="${preview}" alt="${description}" />
+</a>
+`
+}).join("");
+}
+
+function onImgContainerClick (event){
+    
+    event.preventDefault();
+
+    const isGalleryImgEl = event.target.classList.contains('gallery__image');
+    if(!isGalleryImgEl){
+        return;
+    }
+
+    new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250});
+
+}
+
