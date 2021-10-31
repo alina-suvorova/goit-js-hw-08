9baa61a85f74f590127b75329703194d6ff3b135
Alina <alinasuv@gmail.com>
2021-10-31 12:27:23 +0300

    Добавляет 2 задание

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,9 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const localeStorageKey = "videoplayer-current-time";
+
+player.on('timeupdate', throttle(data => localStorage.setItem(localeStorageKey, data.seconds), 1000));
+player.setCurrentTime(localStorage.getItem(localeStorageKey));
