96244ad7b025a3dd2314ced83022ffe1f03ef2eb
Alina <alinasuv@gmail.com>
2021-10-31 12:56:18 +0300

    Добавляет 3 задание

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
+
+  const form = document.querySelector('.feedback-form');
+  const input = document.querySelector('input');
+  const textarea = document.querySelector('textarea');
+  const storageValue = {
+    email: "",
+    message: "",
+  };
+  const localeStorageKey = "feedback-form-state";
+  
+  input.addEventListener('input', throttle(onEmailInput, 500));
+  textarea.addEventListener('input', throttle(onMessageInput, 500));
+  form.addEventListener('submit', onFormSubmit);
+
+function onEmailInput(event) {
+   storageValue.email = event.target.value;
+   localStorage.setItem(localeStorageKey, JSON.stringify(storageValue));
+};
+
+function onMessageInput(event) {
+    storageValue.message = event.target.value;
+    localStorage.setItem(localeStorageKey, JSON.stringify(storageValue));
+};
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    
+    if (localStorage.getItem(localeStorageKey)) {
+        console.log(localStorage.getItem(localeStorageKey));
+    }
+}
+
+
+    
+    
 
